0a97f2e4f41f5b493945fed9a7a20c568f5a69b9
YS <15317724+yongyongsheng@users.noreply.github.com>
2023-09-09 08:03:17 +0800

    Update __init__.py

--- function/__init__.py
+++ function/__init__.py
@@ -1,5 +1,5 @@
 import logging
-import azure.functions as functions
+import azure.functions as func
 
 def main(req: func.HttpRequest) -> func.HttpResponse: 
     logging.info('Python HTTP trigger function processed a request.')
